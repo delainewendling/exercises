8a61e761c7dcc3726dba8befc4350a05c2278e72
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-14 11:15:16 -0500

    Got buttons and colors working

--- the-static-web/converter/converter.js
+++ the-static-web/converter/converter.js
@@ -1,32 +1,74 @@
+
+// Converted Temperature
+var convertedTemp = document.getElementById("convertedTemp");
+
+// Get a reference to the button element in the DOM
+var buttonSubmit = document.getElementById("converter");
+var buttonClear = document.getElementById("clear");
+
+//Function that converts to Celsius
 function toCelsius () {
-  var tempValue = document.getElementById('temperature').value;
-  var celsius = Math.round(tempValue*(9/5)+32);
+  var tempValue = Number(document.getElementById('temperature').value);
+  var celsius = Math.round((tempValue-32)*(5/9));
   return celsius;
 }
 
+//Function that converts to Fahrenheit
 function toFahrenheit () {
-  var tempValue = document.getElementById('temperature').value;
-  var fahrenheit = Math.round((tempValue-32)*(5/9));
+  var tempValue = Number(document.getElementById('temperature').value);
+  var fahrenheit = Math.round(tempValue*(9/5)+32);
   return fahrenheit;
 }
-
-// Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
+  clickEvent.preventDefault();
   if(document.getElementById('celsius').checked) {
-    document.getElementById('convertedTemp').innerHTML(toCelsius());
+    var temp = toCelsius();
+    if (temp > 32) {
+      convertedTemp.innerHTML += "<p> It's hot outside! </p>";
+      convertedTemp.style.backgroundColor = "rgba(217, 53, 49, .8)";
+    } else if (temp < 0) {
+      convertedTemp.innerHTML += "<p> Brrrr it's cold </p>";
+      convertedTemp.style.backgroundColor = "rgba(40, 102, 123, .7)";
+    }
   } else if (document.getElementById('fahrenheit').checked) {
-    document.getElementById('convertedTemp').innerHTML(toFahrenheit());
-  } 
+    var temp = toFahrenheit();
+    if (temp > 90) {
+      convertedTemp.innerHTML += "<p> It's hot outside! </p>";
+      convertedTemp.style.backgroundColor = "rgba(217, 53, 49, .8)";
+    } else if (temp < 32) {
+      convertedTemp.innerHTML += "<p> Brrrr it's cold </p>";
+      convertedTemp.style.backgroundColor = "rgba(40, 102, 123, .7)";
+    }
+  }
+convertedTemp.innerHTML += "<h4> The temperature would be: " + temp + "&deg; </h4>";
+convertedTemp.style.border = "2px solid #999"
   console.log("event", clickEvent);
 }
 
-// Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+//Function that clears the form
+function clearForm (clear) {
+  document.getElementById("convertedTemp").innerHTML = " ";
+  document.getElementById("convertedTemp").style.border = "none";
+  document.getElementById("convertedTemp").style.backgroundColor = "#fff";
+}
 
+
+
+// Assign a function to be executed when the button is clicked
+buttonSubmit.addEventListener("click", determineConverter);
+buttonClear.addEventListener("click", clearForm);
+
+
+
+// document.getElementById("temperature").addEventListener("keydown", function(event) {
+//     if (event.keyCode === 13) {
+//         event.preventDefault();
+//         document.getElementById("converter").click();
+//     }
+// });
 // input.addEventListener("keydown", function (event) {
 //   VK_RETURN
 //   if (event.defaultPrevented) {
