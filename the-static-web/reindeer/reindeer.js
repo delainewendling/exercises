86d1a1a85ecbf150bea561859c1e774ddcd4bc0d
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-22 15:52:35 -0500

    Modified reindeer file

--- the-static-web/reindeer/reindeer.js
+++ the-static-web/reindeer/reindeer.js
@@ -7,7 +7,7 @@ var hohohoElement = document.getElementById("coloredReindeer");
 var cLength = colors.length;
 var rLength = reindeer.length;
 
-for(i=0; i<cLength; i++) {
+for(i=0; i<Math.max(cLength, rLength); i++) { //You can add a function that tells you the maximum length if you were to be unaware of the lengths of the arrays
   if (i >= rLength) {
     var remainder = i % rLength;
     hohohoElement.innerHTML += "<p>"+ colors[i] + " " + reindeer[remainder]+ "</p>";
@@ -15,3 +15,15 @@ for(i=0; i<cLength; i++) {
     hohohoElement.innerHTML += "<p>"+ colors[i] + " " + reindeer[i]+ "</p>";
  }
 }
+
+
+
+//Minified Code
+
+for(i=0; i<Math.max(cLength, rLength); i++) { //You can add a function that tells you the maximum length if you were to be unaware of the lengths of the arrays
+    hohohoElement.innerHTML += "<p>"+ colors[i % cLength] + " " + reindeer[i % rLength]+ "</p>";
+
+}
+
+
+
