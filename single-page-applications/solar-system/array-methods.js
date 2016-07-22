61b3ad7099c30db800a30c1ba904da966a030ca4
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-22 15:18:06 -0500

    Finished all four methods

--- single-page-applications/solar-system/array-methods.js
+++ single-page-applications/solar-system/array-methods.js
@@ -3,17 +3,38 @@ var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus
 
 var capitalized = planets.map((element)=>{
     return element.substring(0,1).toUpperCase() + element.substring(1);
-}).forEach((element)=>{
+})
+
+capitalized.forEach((element)=>{
   document.getElementById("planetContainer").innerHTML +=
   `<p> ${element} </p>`;
 })
 
-console.log(capitalized)
+var filteredPlanets = planets.filter((element)=>{
+  return element.includes("e");
+}).map((element)=>{
+  return element.substring(0,1).toUpperCase() + element.substring(1);
+})
+
+console.log(filteredPlanets);
+
+filteredPlanets.forEach((element)=>{
+  document.getElementById("ePlanets").innerHTML +=
+    `<p> ${element} </p>`
+})
+
+var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
+
+var sentence = words.reduce((previousValue, currentValue)=> {
+  return previousValue + " " + currentValue;
+})
+
+document.getElementById("randomSentence").innerHTML +=
+    `<p> ${sentence} </p>`
+
 
 // Use the map method to create a new array where the first letter of each planet is capitalized
 
 // Use the filter method to create a new array that contains planets with the letter 'e'
 
 // Use the reduce method to create a sentence from the words in the following array
-
-var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
