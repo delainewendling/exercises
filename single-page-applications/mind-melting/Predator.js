4c1be5a46107906b5872a8cdeb54e86af3e36e1a
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-27 10:25:01 -0500

    Finished functions to add data to the DOM

--- single-page-applications/mind-melting/Predator.js
+++ single-page-applications/mind-melting/Predator.js
@@ -1,30 +1,33 @@
 var Predator = (function () {
-  var carnivores = [];
+  var carnivores = []; //private variables, naming convention says to place the underscore in front
   var herbivores = [];
 
-  return {
-    loadHerbivores: function (callback) {
+  return { //public interface, all functions below are public
+    loadHerbivores: function (cb) {
       var loader = new XMLHttpRequest();
       loader.open("GET", "herbivores.json")
-      loader.addEventListener("load", function () {
+      loader.addEventListener("load", function (evt) {
         // Set the value of the private array
-        herbivores = JSON.parse(this.responseText);
-        callback(herbivores)
+        herbivores = JSON.parse(evt.target.responseText); //you can use evt.target because that is the thing you are targeting here
+        cb(herbivores)
       });
+      loader.send()
     },
-    loadCarnivores: function (callbackToInvoke) {
+    loadCarnivores: function (cb) {
       var loader = new XMLHttpRequest();
       loader.open("GET", "carnivores.json")
       loader.addEventListener("load", function () {
         // Set the value of the private array
-        carnivores = JSON.parse(this.responseText);
+        carnivores = JSON.parse(this.responseText);//this is a reference to the execution context. It is different in different contexts.
 
         // Invoke the callback function so that the caller knows
         // that the process is complete. Make sure to pass the
         // carnivore array as an argument.
-        callbackToInvoke(carnivores)
+        cb(carnivores)
 
       });
+      loader.send();
     }
   }
 })(Predator || {});
+
--- single-page-applications/mind-melting/iife-xhr.js
+++ single-page-applications/mind-melting/iife-xhr.js
@@ -1,16 +1,28 @@
 var Predator = (function(animals) {
 
 
-function showCarnivores (carnivores) {
-  var carnivores = carnivores. 
+function showCarnivores (carnivores) //_carnivores is now the array that we created in the Predator.js file
+ {
+  var carnivoreContainer = document.querySelector(".carnivores");
+  console.log("carnivores", carnivores)
+  var carnivore = carnivores.carnivores;
+  carnivore.forEach((carn)=>{
+    carnivoreContainer.innerHTML += `<li> Name: ${carn.name} </li>
+      <li> Family: ${carn.family} </li>`
+  });
 
 }
 
 function showHerbivores (herbivores) {
-
+  var herbivoreContainer = document.querySelector(".herbivores");
+  var herbivore = herbivores.herbivores;
+  herbivore.forEach((herb) =>{
+    herbivoreContainer.innerHTML += `<li> Name: ${herb.name} </li>
+      <li> Family: ${herb.family} </li>`
+  });
 }
 
-Predator.loadCarnivores(showCarnivores);
+Predator.loadCarnivores(showCarnivores); //We're passing the function showCarnivores into the loadCarnivores function
 Predator.loadHerbivores(showHerbivores);
 
 }) (Predator || {});
