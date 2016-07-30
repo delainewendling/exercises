cf7ba9114a964ee533b2d8aabde3fd44e4329152
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-30 11:57:00 -0500

    Finished chaining methods exercise

--- single-page-applications/chaining-array-methods/chaining.js
+++ single-page-applications/chaining-array-methods/chaining.js
@@ -1,13 +1,16 @@
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-var new1 = integers.sort(function(a,b){
+var newArray = integers.sort(function(a,b){
   return b-a;
-});
-var newer = new1.filter(function(integers){
+}).filter(function(integers){
   return integers < 19
-});
+}).map(function(remaining){
+  return remaining*1.5-1
+}).reduce(function(prev, curr){
+  return prev+curr;
+})
 
-console.log(newer);
+console.log(newArray);
 
 // Sort the numbers in descending order (10, 9, 8, 7, etc).
 // Remove any integers greater than 19.
