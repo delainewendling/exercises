9234a268e4c62b1424d8b048ba3d0876413c35ac
Delaine Wendling <delaine.wendling@gmail.com>
2016-08-01 15:31:24 -0500

    Want to save before refactoring

--- modern-javascript-developer/slideshow/script.js
+++ modern-javascript-developer/slideshow/script.js
@@ -10,38 +10,6 @@ $(function(){
     $(target).find(".caption").addClass("hidden")
   })
 
-//   $('#arrowLeft').click((e)=>{
-//     if(index <= 0){
-//       index=6;
-//       console.log("left index w/ reset")
-//     }
-//     var remainder = index%numberOfImages;
-//     if (remainder === 0){
-//       $('.imgContainer').eq(remainder).addClass('hidden')
-//       $('.imgContainer').eq(5).removeClass('hidden')
-//       index--
-//    } else {
-//       $('.imgContainer').eq(remainder).addClass('hidden')
-//       $('.imgContainer').eq(remainder).prev(".imgContainer").removeClass('hidden')
-//       index--
-//    }
-//   })
-//    $('#arrowRight').click((e)=>{
-//     var remainder = index%numberOfImages;
-//     if (remainder === 5){
-//       $('.imgContainer').eq(remainder).addClass("hidden")
-//       $('.imgContainer').eq(0).removeClass("hidden")
-//       index++
-//     } else {
-//       $('.imgContainer').eq(remainder).addClass("hidden")
-//       $('.imgContainer').eq(remainder).next('.imgContainer').removeClass("hidden")
-//       index++
-//     }
-//   })
-// });
-
-
-  //If left arrow is clicked
   $('#arrowLeft').click((e)=>{
     if(index <= 0){
       index=6;
@@ -49,42 +17,74 @@ $(function(){
     }
     var remainder = index%numberOfImages;
     if (remainder === 0){
-      $('.imgContainer').eq(remainder).fadeOut(500,function() {
-        $('.imgContainer').eq(remainder).addClass('hidden')
-        $('.imgContainer').eq(5).fadeIn(0, function(){
-          $('.imgContainer').eq(5).removeClass("hidden")
-        })
-      })
+      $('.imgContainer').eq(remainder).addClass('hidden')
+      $('.imgContainer').eq(5).removeClass('hidden')
       index--
    } else {
-      $('.imgContainer').eq(remainder).fadeOut(500,function() {
-        $('.imgContainer').eq(remainder).addClass("hidden");
-        $('.imgContainer').eq(remainder).prev(".imgContainer").fadeIn(0, function(){
-          $('.imgContainer').eq(remainder).prev(".imgContainer").removeClass("hidden")
-        })
-      })
+      $('.imgContainer').eq(remainder).addClass('hidden')
+      $('.imgContainer').eq(remainder).prev(".imgContainer").removeClass('hidden')
       index--
    }
   })
-  //If right arrow is clicked
-  $('#arrowRight').click((e)=>{
+   $('#arrowRight').click((e)=>{
     var remainder = index%numberOfImages;
     if (remainder === 5){
-      $('.imgContainer').eq(remainder).fadeOut(500,function(){
-        $('.imgContainer').eq(remainder).addClass("hidden");
-        $('.imgContainer').eq(0).fadeIn(0, function(){
-          $('.imgContainer').eq(0).removeClass("hidden")
-        })
-      })
+      $('.imgContainer').eq(remainder).addClass("hidden")
+      $('.imgContainer').eq(0).removeClass("hidden")
       index++
     } else {
-      $('.imgContainer').eq(remainder).fadeOut(500,function(){
-        $('.imgContainer').eq(remainder).addClass("hidden");
-        $('.imgContainer').eq(remainder).next('.imgContainer').fadeIn(0, function(){
-        $('.imgContainer').eq(remainder).next('.imgContainer').removeClass("hidden")
-        })
-      })
+      $('.imgContainer').eq(remainder).addClass("hidden")
+      $('.imgContainer').eq(remainder).next('.imgContainer').removeClass("hidden")
       index++
     }
   })
+
+
+  //   //If left arrow is clicked
+  // $('#arrowLeft').click((e)=>{
+  //   if(index <= 0){
+  //     index=6;
+  //     console.log("left index w/ reset")
+  //   }
+  //   var remainder = index%numberOfImages;
+  //   if (remainder === 0){
+  //     $('.imgContainer').eq(remainder).fadeOut(500,function() {
+  //       $('.imgContainer').eq(remainder).addClass('hidden')
+  //       $('.imgContainer').eq(5).fadeIn(0, function(){
+  //         $('.imgContainer').eq(5).removeClass("hidden")
+  //       })
+  //     })
+  //     index--
+  //  } else {
+  //     $('.imgContainer').eq(remainder).fadeOut(500,function() {
+  //       $('.imgContainer').eq(remainder).addClass("hidden");
+  //       $('.imgContainer').eq(remainder).prev(".imgContainer").fadeIn(0, function(){
+  //         $('.imgContainer').eq(remainder).prev(".imgContainer").removeClass("hidden")
+  //       })
+  //     })
+  //     index--
+  //  }
+  // })
+  // //If right arrow is clicked
+  // $('#arrowRight').click((e)=>{
+  //   var remainder = index%numberOfImages;
+  //   if (remainder === 5){
+  //     $('.imgContainer').eq(remainder).fadeOut(500,function(){
+  //       $('.imgContainer').eq(remainder).addClass("hidden");
+  //       $('.imgContainer').eq(0).fadeIn(0, function(){
+  //         $('.imgContainer').eq(0).removeClass("hidden")
+  //       })
+  //     })
+  //     index++
+  //   } else {
+  //     $('.imgContainer').eq(remainder).fadeOut(500,function(){
+  //       $('.imgContainer').eq(remainder).addClass("hidden");
+  //       $('.imgContainer').eq(remainder).next('.imgContainer').fadeIn(0, function(){
+  //       $('.imgContainer').eq(remainder).next('.imgContainer').removeClass("hidden")
+  //       })
+  //     })
+  //     index++
+  //   }
+  // })
+
 });
