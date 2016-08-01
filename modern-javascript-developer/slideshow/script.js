b227104426f4bbe535413436bf38871e2ba7be5e
Delaine Wendling <delaine.wendling@gmail.com>
2016-08-01 14:45:31 -0500

    Added captions on hover

--- modern-javascript-developer/slideshow/script.js
+++ modern-javascript-developer/slideshow/script.js
@@ -1,6 +1,14 @@
 $(function(){
   var numberOfImages = $('.imgContainer').length
   var index = 0;
+  $('.imgContainer').mouseenter((e)=>{
+    var target = $(e.currentTarget)
+    $(target).find(".caption").removeClass("hidden")
+  })
+  $('.imgContainer').mouseleave((e)=>{
+    var target = $(e.currentTarget)
+    $(target).find(".caption").addClass("hidden")
+  })
   $('#arrowLeft').click((e)=>{
     if(index <= 0){
       index=6;
@@ -30,3 +38,42 @@ $(function(){
     }
   })
 });
+
+// $(function(){
+//   var numberOfImages = $('.imgContainer').length
+//   var index = 0;
+//   //If left arrow is clicked
+//   $('#arrowLeft').click((e)=>{
+//     if(index <= 0){
+//       index=6;
+//       console.log("left index w/ reset")
+//     }
+//     var remainder = index%numberOfImages;
+//     if (remainder === 0){
+//       $('.imgContainer').eq(remainder).fadeOut( function() {
+//         $('.imgContainer').eq(5).removeClass("hidden")
+//         index--
+//       })
+//    } else {
+//       $('.imgContainer').eq(remainder).fadeOut( function() {
+//       $('.imgContainer').eq(remainder).prev(".imgContainer").removeClass("hidden")
+//        index--
+//       })
+//    }
+//   })
+//   //If right arrow is clicked
+//   $('#arrowRight').click((e)=>{
+//     var remainder = index%numberOfImages;
+//     if (remainder === 5){
+//       $('.imgContainer').eq(remainder).fadeOut(function(){
+//         $('.imgContainer').eq(0).removeClass("hidden")
+//         index++
+//       })
+//     } else {
+//       $('.imgContainer').eq(remainder).fadeOut(function(){
+//         $('.imgContainer').eq(remainder).next('.imgContainer').removeClass("hidden")
+//         index++
+//       })
+//     }
+//   })
+// });
