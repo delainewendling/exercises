b0dacaec51058eac2898daed65940f68872219c6
Delaine Wendling <delaine.wendling@gmail.com>
2016-08-01 17:58:39 -0500

    created navbar

---
+++ modern-javascript-developer/giphy_generator/load_trending.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  var API_URL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC"
+  $.getJSON(API_URL, function(res){
+    console.log("loaded")
+    var images = res.data;
+    images.forEach(function(img){
+      $('.imgContainer').append(`<img src="${img.images.original.url}" class="giphy">`);
+    })
+    // $("img").first().addClass("active")
+  })
+})
