53ab848f1b38ecc95ebf6435cb21efcd2af31c1f
Delaine Wendling <delaine.wendling@gmail.com>
2016-08-01 15:14:32 -0500

    Added fade in and fade out

--- modern-javascript-developer/slideshow/script.js
+++ modern-javascript-developer/slideshow/script.js
@@ -9,40 +9,7 @@ $(function(){
     var target = $(e.currentTarget)
     $(target).find(".caption").addClass("hidden")
   })
-  $('#arrowLeft').click((e)=>{
-    if(index <= 0){
-      index=6;
-      console.log("left index w/ reset")
-    }
-    var remainder = index%numberOfImages;
-    if (remainder === 0){
-      $('.imgContainer').eq(remainder).addClass('hidden')
-      $('.imgContainer').eq(5).removeClass('hidden')
-      index--
-   } else {
-      $('.imgContainer').eq(remainder).addClass('hidden')
-      $('.imgContainer').eq(remainder).prev(".imgContainer").removeClass('hidden')
-      index--
-   }
-  })
-   $('#arrowRight').click((e)=>{
-    var remainder = index%numberOfImages;
-    if (remainder === 5){
-      $('.imgContainer').eq(remainder).addClass("hidden")
-      $('.imgContainer').eq(0).removeClass("hidden")
-      index++
-    } else {
-      $('.imgContainer').eq(remainder).addClass("hidden")
-      $('.imgContainer').eq(remainder).next('.imgContainer').removeClass("hidden")
-      index++
-    }
-  })
-});
 
-// $(function(){
-//   var numberOfImages = $('.imgContainer').length
-//   var index = 0;
-//   //If left arrow is clicked
 //   $('#arrowLeft').click((e)=>{
 //     if(index <= 0){
 //       index=6;
@@ -50,30 +17,74 @@ $(function(){
 //     }
 //     var remainder = index%numberOfImages;
 //     if (remainder === 0){
-//       $('.imgContainer').eq(remainder).fadeOut( function() {
-//         $('.imgContainer').eq(5).removeClass("hidden")
-//         index--
-//       })
+//       $('.imgContainer').eq(remainder).addClass('hidden')
+//       $('.imgContainer').eq(5).removeClass('hidden')
+//       index--
 //    } else {
-//       $('.imgContainer').eq(remainder).fadeOut( function() {
-//       $('.imgContainer').eq(remainder).prev(".imgContainer").removeClass("hidden")
-//        index--
-//       })
+//       $('.imgContainer').eq(remainder).addClass('hidden')
+//       $('.imgContainer').eq(remainder).prev(".imgContainer").removeClass('hidden')
+//       index--
 //    }
 //   })
-//   //If right arrow is clicked
-//   $('#arrowRight').click((e)=>{
+//    $('#arrowRight').click((e)=>{
 //     var remainder = index%numberOfImages;
 //     if (remainder === 5){
-//       $('.imgContainer').eq(remainder).fadeOut(function(){
-//         $('.imgContainer').eq(0).removeClass("hidden")
-//         index++
-//       })
+//       $('.imgContainer').eq(remainder).addClass("hidden")
+//       $('.imgContainer').eq(0).removeClass("hidden")
+//       index++
 //     } else {
-//       $('.imgContainer').eq(remainder).fadeOut(function(){
-//         $('.imgContainer').eq(remainder).next('.imgContainer').removeClass("hidden")
-//         index++
-//       })
+//       $('.imgContainer').eq(remainder).addClass("hidden")
+//       $('.imgContainer').eq(remainder).next('.imgContainer').removeClass("hidden")
+//       index++
 //     }
 //   })
 // });
+
+
+  //If left arrow is clicked
+  $('#arrowLeft').click((e)=>{
+    if(index <= 0){
+      index=6;
+      console.log("left index w/ reset")
+    }
+    var remainder = index%numberOfImages;
+    if (remainder === 0){
+      $('.imgContainer').eq(remainder).fadeOut(500,function() {
+        $('.imgContainer').eq(remainder).addClass('hidden')
+        $('.imgContainer').eq(5).fadeIn(0, function(){
+          $('.imgContainer').eq(5).removeClass("hidden")
+        })
+      })
+      index--
+   } else {
+      $('.imgContainer').eq(remainder).fadeOut(500,function() {
+        $('.imgContainer').eq(remainder).addClass("hidden");
+        $('.imgContainer').eq(remainder).prev(".imgContainer").fadeIn(0, function(){
+          $('.imgContainer').eq(remainder).prev(".imgContainer").removeClass("hidden")
+        })
+      })
+      index--
+   }
+  })
+  //If right arrow is clicked
+  $('#arrowRight').click((e)=>{
+    var remainder = index%numberOfImages;
+    if (remainder === 5){
+      $('.imgContainer').eq(remainder).fadeOut(500,function(){
+        $('.imgContainer').eq(remainder).addClass("hidden");
+        $('.imgContainer').eq(0).fadeIn(0, function(){
+          $('.imgContainer').eq(0).removeClass("hidden")
+        })
+      })
+      index++
+    } else {
+      $('.imgContainer').eq(remainder).fadeOut(500,function(){
+        $('.imgContainer').eq(remainder).addClass("hidden");
+        $('.imgContainer').eq(remainder).next('.imgContainer').fadeIn(0, function(){
+        $('.imgContainer').eq(remainder).next('.imgContainer').removeClass("hidden")
+        })
+      })
+      index++
+    }
+  })
+});
