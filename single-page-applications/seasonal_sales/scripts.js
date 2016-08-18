af3ed2860c82562cc575f3a16069c8d467424219
Delaine Wendling <delaine.wendling@gmail.com>
2016-08-17 22:07:52 -0500

    Finished exercise

--- single-page-applications/seasonal_sales/scripts.js
+++ single-page-applications/seasonal_sales/scripts.js
@@ -2,19 +2,48 @@ var SeasonalSales = (function(sales){
 
   sales.printItems = function(products, categories){
     console.log("products", products)
-    products.forEach((product)=>{
-    $(".productsContainer").append(`<div class="product col-xs-4"><h3> ${product.name}</h3><h4 class="price"> Price: $${product.price}</h4><p> Seasonally on Sale in: ${categories[product.category_id-1].season_discount}</p></div>`)
+    var results = ""
+    products.forEach((product, i)=>{
+      if (i%3 === 0){
+        results += `<div class="row row-eq-height">`
+      }
+      results += `<div class="product col-xs-4"><h3> ${product.name}</h3><h4 class="price"> Price: $${product.price}</h4><p> Seasonally on Sale in: <strong class="season"> ${categories[product.category_id-1].season_discount} </strong></p></div>`
+      if ((i+1)%3===0){
+        results+=`</div>`
+      }
     })
+    $(".productsContainer").append(results)
     selectedItem(products, categories)
   }
 
   function selectedItem (products, categories) {
-    $(".dropdown-menu li a").click(function(e){
-      console.log("id", $(this).attr('id'))
-    // $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-    // $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-  });
+    $(".dropdown-menu li").click(function(e){
+      discountedPrices(products, categories, $(this).attr('id'))
+    });
   }
+
+  function discountedPrices (products, categories, season) {
+    var newProducts = ""
+    var newPrice;
+    products.forEach((product)=>{
+    $(".productsContainer").html("")
+    if(`${product.category_id}`=== season){
+      newPrice = Math.round(product.price*(1-categories[product.category_id-1].discount)*100)/100
+      discount = categories[product.category_id-1].discount*100
+      newProducts += `<div class="onSaleNow col-xs-4"><h3> ${product.name}</h3><h4 class="price" style="text-decoration: line-through"> Price: $${product.price}</h4><h4 class="newPrice"> $${newPrice}</h4><p class="onSale"> <strong> Discounted ${discount}% right now! </strong></p></div>`
+    } else {
+      newProducts += `<div class="product col-xs-4"><h3> ${product.name}</h3><h4 class="price"> Price: $${product.price}</h4><p class="newPrice"></p><p > Seasonally on Sale in: <strong class="season"> ${categories[product.category_id-1].season_discount} </strong></p></div>`
+    }
+  })
+    $(".productsContainer").append(newProducts)
+  }
+    // var seasons = document.querySelectorAll(".season")
+    // seasons.forEach((seasonItem)=>{
+    //   if(seasonItem.innerText === season){
+    //     console.log("season is", seasonItem)
+    //     season
+    //   }
+    // })
 
   return sales
 })(SeasonalSales || {})
